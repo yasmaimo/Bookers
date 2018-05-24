827d07a433dda8911981c330759122bffe98fa59
yasmaimo <yasmaimo0141@gmail.com>
2018-05-24 18:45:14 +0900

    resubmit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,23 +14,27 @@ class BooksController < ApplicationController
 
   def create
   	  book = Book.new(book_params)
-  	  book.save
-  	  redirect_to book_path(book)
-  	  flash[:created] = "Book was successfully created."
+  	  if book.save
+    	  redirect_to book_path(book)
+    	  flash[:created] = "感想を投稿しました"
+      else
+        redirect_to books_path(book)
+        flash[:failed] = "タイトルと本文を入力してください"
+      end
   end
 
   def update
       book = Book.find(params[:id])
       book.update(book_params)
       redirect_to book_path(book)
-      flash[:updated] = "Book was successfully updated."
+      flash[:updated] = "投稿を編集しました"
   end
 
   def destroy
   	  book = Book.find(params[:id])
   	  book.destroy
   	  redirect_to books_path
-  	  flash[:destroyed] = "Book was successfully destroyed."
+  	  flash[:destroyed] = "投稿を削除しました"
   end
 
   private
